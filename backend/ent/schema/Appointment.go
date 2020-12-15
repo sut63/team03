06c1302414255073aa9fe41491eeffee2199e890
jeffair31 <anuthida3101@gmail.com>
2020-12-15 20:28:34 +0700

    ทำ Entity ของระบบการนัดหมายผู้ป่วย - close #16

---
+++ backend/ent/schema/Appointment.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
+
+// Appointment holds the schema definition for the Appointment entity.
+type Appointment struct {
+	ent.Schema
+}
+
+// Fields of the Appointment.
+func (Appointment) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("detail").NotEmpty(),
+		field.Time("datetime"),
+	}
+}
+
+// Edges of the Appointment.
+func (Appointment) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("patient", Patient.Type).Ref("appointment").Unique(),
+		edge.From("room", Room.Type).Ref("appointment").Unique(),
+		edge.From("dentist", Dentist.Type).Ref("appointment").Unique(),
+	}
+}
