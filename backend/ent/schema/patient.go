c4175671d0e4ca3428408fe2c41e1e29359708fc
Thitaparn <thitapornpetsaeng@gmail.com>
2021-01-07 21:14:50 +0700

    แก้ไข Entity ของระบบประวัติผู้ป่วย - close #49

--- backend/ent/schema/patient.go
+++ backend/ent/schema/patient.go
@@ -14,10 +14,11 @@ type Patient struct {
 // Fields of the Patient.
 func (Patient) Fields() []ent.Field {
 	return []ent.Field{
+		field.String("patient_ID").NotEmpty().Unique(),
 		field.String("name").NotEmpty(),
 		field.String("cardID").NotEmpty(),
 		field.String("tel").NotEmpty(),
-		field.Int("age"),
+		field.Int("age").Positive(),
 		field.Time("birthday"),
 	}
 }
@@ -25,8 +26,10 @@ func (Patient) Fields() []ent.Field {
 // Edges of the Patient.
 func (Patient) Edges() []ent.Edge {
 	return []ent.Edge{
-		edge.From("gender", Gender.Type).Ref("patients").Unique(),          
-		edge.From("medicalcare", MedicalCare.Type).Ref("patients").Unique(), 
-		edge.From("employee", Employee.Type).Ref("patients").Unique(),     
-		edge.From("disease", Disease.Type).Ref("patients").Unique(),   
+		edge.From("gender", Gender.Type).Ref("patients").Unique(),
+		edge.From("medicalcare", MedicalCare.Type).Ref("patients").Unique(),
+		edge.From("nurse", Nurse.Type).Ref("patients").Unique(), 
+		edge.From("disease", Disease.Type).Ref("patients").Unique(),
 	}
+
+}
