60386a0b178acde4c98cc369ba7e0be82469b32a
kaews <kaewsaismilez@gmail.com>
2021-01-09 17:09:28 +0700

    แก้ไข entity nurse - close #118

--- backend/ent/schema/nurse.go
+++ backend/ent/schema/nurse.go
@@ -29,6 +29,7 @@ func (Nurse) Edges() []ent.Edge {
 		edge.To("queue", Queue.Type).StorageKey(edge.Column("nurse_id")),
 		edge.To("medicalfiles", Medicalfile.Type).StorageKey(edge.Column("nurse_id")),
 		edge.To("dentalexpenses", Dentalexpense.Type).StorageKey(edge.Column("nurse_id")),
+		edge.To("patients", Patient.Type).StorageKey(edge.Column("nurse_id")),
 		
 	}
 }
