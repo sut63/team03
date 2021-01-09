4de22c1a40beae32ec5f83fe3b915e92cfe86b71
kaews <kaewsaismilez@gmail.com>
2021-01-09 20:43:15 +0700

    แก้ไข entity gender - close #125

--- backend/ent/schema/Gender.go
+++ backend/ent/schema/Gender.go
@@ -21,6 +21,7 @@ func (Gender) Fields() []ent.Field {
 // Edges of the Gender.
 func (Gender) Edges() []ent.Edge {
     return []ent.Edge{
-        edge.To("patients", Patient.Type).StorageKey(edge.Column("gender_id")),
+		edge.To("patients", Patient.Type).StorageKey(edge.Column("gender_id")),
+		edge.To("dentists", Dentist.Type).StorageKey(edge.Column("gender_id")),
     }
 }
