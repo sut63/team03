7920265c3a3bd29716178ed94c351237be57c78d
pawor <pawor.emi@gmail.com>
2021-01-27 21:43:04 +0700

    ทำ UI ของระบบค้นหารายการค่ารักษา  - close #271

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -10,6 +10,7 @@ import Dentist from './components/SaveDentist';
 import Queue from './components/SaveQueue';
 import Nurse from './components/SaveNurse';
 import Welcome from './components/Welcome';
+import SearchDenExpen from './components/SearchDenExpen';
 
 export const plugin = createPlugin({
   id: 'welcome',
@@ -25,5 +26,6 @@ export const plugin = createPlugin({
     router.registerRoute('/SaveDentist', Dentist);
     router.registerRoute('/SaveQueue', Queue);
     router.registerRoute('/SaveNurse', Nurse);
+    router.registerRoute('/SearchDenExpen', SearchDenExpen);
   }
 });
